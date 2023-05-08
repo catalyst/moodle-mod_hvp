68a3b36048df82f9b7f6950b132bc3566a6f0f1c
Matthew Hilton <matthewhilton@catalyst-au.net>
2023-05-08 13:59:28 +1000

    [#38] Fix css filepaths

--- lib.php
+++ lib.php
@@ -353,12 +353,16 @@ function hvp_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload
                 return false; // Invalid context.
             }
 
-            $filename = array_pop($args);
-            $filepath = (!$args ? '/' : '/' .implode('/', $args) . '/');
-            $path = $CFG->dirroot . '/mod/hvp' . $filepath . $filename;
-            $cssfile = realpath($path);
+            $relativefilepath = '/' . implode('/', $args);
 
-            css_send_cached_css($cssfile, get_config('mod_hvp', 'version'));
+            // Ensure the path begins with /mod/hvp.
+            if (!str_contains($relativefilepath, '/mod/hvp')) {
+                $relativefilepath = '/mod/hvp' . $relativefilepath;
+            }
+
+            $absolutefilepath = realpath($CFG->dirroot . $relativefilepath);
+
+            css_send_cached_css($absolutefilepath, get_config('mod_hvp', 'version'));
             exit();
     }
 
