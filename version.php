1b9b0adbec28f9ae859a4362bcd0793dbe91b80b
Alexander Van der Bellen <alexandervanderbellen@catalyst-au.net>
2024-12-10 21:06:52 +0800

    Update plugin supported number

--- version.php
+++ version.php
@@ -29,4 +29,4 @@ $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.28.2';
-$plugin->supported = [400, 404];
+$plugin->supported = [401, 404];
