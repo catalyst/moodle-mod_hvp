53e526e05dd9050c3aa58d83980ee73c7580a080
Hoang, Minh Duc <minh-duc.hoang@fernuni-hagen.de>
2025-03-10 14:45:01 +0100

    Fix merge conflict

--- locallib.php
+++ locallib.php
@@ -620,7 +620,11 @@ function hvp_send_notification_messages($course, $hvp, $attempt, $context, $cm) 
     // Check for notifications required.
     $notifyfields = 'u.id, u.username, u.idnumber, u.email, u.emailstop, u.lang,
             u.timezone, u.mailformat, u.maildisplay, u.auth, u.suspended, u.deleted, ';
-    $notifyfields .= 'u.' . implode(', u.', \core_user\fields::get_name_fields());
+    if (class_exists('\core_user\fields')) {
+        $notifyfields .= \core_user\fields::for_name()->get_sql('u', false, '', '', false)->selects;
+    } else {
+        $notifyfields .= get_all_user_name_fields(true, 'u');
+    }
     $groups       = groups_get_all_groups($course->id, $submitter->id, $cm->groupingid);
     if (is_array($groups) && count($groups) > 0) {
         $groups = array_keys($groups);
