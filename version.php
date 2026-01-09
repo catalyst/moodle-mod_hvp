0d7d378b676e8ad918d2587213df6011b96ccb74
Peter Burnett <peterburnett@catalyst-au.net>
2026-01-09 11:14:15 +1000

    Revert "[#75] Bump minimum to 4.1"
    This reverts commit 1bf0a39ae5792139749e7ee93a330520227d7540.

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2024120900;
-$plugin->requires  = 2022112800; // 4.1.0
+$plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->supported = [401, 404];
+$plugin->supported = [400, 405];
 $plugin->release   = '1.27.2';
